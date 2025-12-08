c54118697a83ce55bfb0700b3134b5827693f33e
kiseronur-cloud <kiseronur@gmail.com>
2025-12-09 01:08:54 +0300

    Fix Swagger UI route with manual config

--- app.py
+++ app.py
@@ -3,7 +3,23 @@ from flask import Flask, request
 from flasgger import Swagger
 
 app = Flask(__name__)
-swagger = Swagger(app)
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,
+            "model_filter": lambda tag: True,
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
+
+swagger = Swagger(app, config=swagger_config)
 
 # Logging yapılandırması
 logging.basicConfig(
